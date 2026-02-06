de779b326aedb80f519caa0e0145e5580cc81433
RysanDeluna <ry.marco.andrade@gmail.com>
2026-02-06 12:28:10 -0300

    configuring zsh

--- config/terminal/zsh.nix
+++ config/terminal/zsh.nix
@@ -3,6 +3,9 @@ let
   myAliases = {
     ll = "ls -l";
     ".." = "cd ..";
+    dot = "cd ~/.dotfiles";
+    edot = "nvim ~/.dotfiles";  # edit dotfiles
+    hmsf = "home-manager switch --flake";
   };
 in
 {
@@ -14,6 +17,19 @@ in
     zsh = {
       enable = true;
       shellAliases = myAliases;
+      enableCompletion = true;
+      autocd = true;
+      autosuggestion.enable = true;
+      oh-my-zsh = {
+        enable = true;
+        theme = "half-life";
+        plugins = [
+          "aliases"
+          "git"
+          "themes"
+          "colored-man-pages"
+        ];
+      };
     };
   };
 }
